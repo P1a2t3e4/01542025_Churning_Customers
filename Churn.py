c457936faeda9630432f19eb90cfebd66e06ba82
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-17 21:13:57 +0000

    Update Churn.py

--- Churn.py
+++ Churn.py
@@ -1,34 +1,69 @@
+
+
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, roc_auc_score
-from tensorflow.keras.models import load_model
+from keras.models import load_model
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+import pickle
 
-# Load the pre-trained model
-model = load_model('churn_model.h5')
+# Load the trained model
+model = load_model('best_model.pkl')
 
-# Load data for the additional features
-data = pd.read_csv(r"C:\\Users\Patricia Okletey\Downloads\CustomerChurn_dataset.csv")
+# Load the trained StandardScaler
+with open('scaled_data.pkl', 'rb') as file:
+    scaled = pickle.load(file)
 
-# Select the relevant features
-selected_features = ['MonthlyCharges', 'tenure', 'TotalCharges', 'Contract', 'PaymentMethod', 'TechSupport', 'OnlineSecurity', 'gender', 'OnlineBackup', 'InternetService']
-X_additional = data[selected_features]
+# Streamlit app
+def main():
+    st.title('Churning  Customer  App')
 
-# Preprocess the additional data
-scaler = StandardScaler()
-X_additional_scaled = scaler.fit_transform(X_additional)
+    # Collect user input
+    tenure = st.slider('Tenure', 0, 90, 50)
+    monthly_charges = st.slider('Monthly Charges', 0.0, 200.0, 100.0)
+    total_charges = st.slider('Total Charges', 0.0, 5000.0, 2500.0)
+    contract = st.select box('Contract', ['month-to-month, 'one year', 'Two years'])
+    online_security = st.selectbox('Online Security', ['No', 'yes', 'No internet service'])
+    payment_method = st.selectbox('Payment Method', ['electronic check', 'mailed check', 'card'])
+    tech_support = st.selectbox('Tech Support', ['No', 'yes', 'No internet service'])
+    internet_service = st.selectbox('Internet Service', ['DSL', 'fiber optic', 'No'])
+    gender = st.radio('Gender', ['Male', 'Female'])
+    online_backup = st.selectbox('Online Backup', ['No', 'yes', 'No internet service'])
+   
+    # Make a prediction
+    if st.button('Feature values for prediction of churn'):
+        # Transform user input
+        user_input = pd.DataFrame({
+            'tenure': [tenure],
+            'MonthlyCharges': [monthly_charges],
+            'TotalCharges': [total_charges],
+            'Contract': [contract],
+            'OnlineSecurity': [online_security],
+            'PaymentMethod': [payment_method],
+            'TechSupport': [tech_support],
+            'InternetService': [internet_service],
+            'OnlineBackup': [online_backup],
+            'gender': [gender]
+        })
 
-# Evaluate the model
-y_pred = model.predict(X_additional_scaled)
-y_pred_binary = (y_pred > 0.5).astype(int)
+        # Encode categorical variables
+        label_encoder = LabelEncoder()
+        categorical_columns = ['Contract', 'OnlineSecurity', 'PaymentMethod', 'TechSupport', 'InternetService', 'OnlineBackup', 'gender']
+        
+        for column in categorical_columns:
+            user_input[column] = label_encoder.fit_transform(user_input[column])
 
-accuracy = accuracy_score(y_test, y_pred_binary)  # Note: You need to define y_test for this evaluation
-auc_score = roc_auc_score(y_test, y_pred)  # Note: You need to define y_test for this evaluation
+        # Scale the input
+        scaled_input = scaled.transform(user_input)
 
-# Streamlit App
-st.title('Churn Prediction App')
+        # Make a prediction
+        prediction = model.predict(scaled_input)
 
-# Display evaluation results
-st.write(f'Accuracy: {accuracy}')
-st.write(f'AUC Score: {auc_score}')
+        # Display the result
+        churn_probability = prediction[0]
+        churn_prediction = 'Yes, ' if churn_probability >= 0.5 else 'No will not  Churn'
+        st.write(f'Churn Probability: {churn_probability}')
+        st.write(f'prediction: {churn_prediction}')
+
+if _name_ == '_main_':
+    main()
+
