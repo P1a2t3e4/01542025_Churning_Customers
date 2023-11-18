a0b8d6ec8119531ba484663520a661951ff3e3ca
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-18 10:16:03 +0000

    Update Churning.py

--- Churning.py
+++ Churning.py
@@ -4,9 +4,16 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Load the best model and preprocessing objects
-best_model = pickle.load(open("best_model .pkl", "rb"))
+best_model = pickle.load(open("best_model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
-label_encoder = pickle.load(open("label_encoder.pkl", "rb"))
+
+# Load the label encoder only if it exists (check if the file exists)
+label_encoder_file = "label_encoder.pkl"
+if os.path.exists(label_encoder_file):
+    label_encoder = pickle.load(open(label_encoder_file, "rb"))
+else:
+    st.error("Label encoder file not found. Make sure to fit and save the label encoder during training.")
+    st.stop()
 
 # Assuming top_features is defined somewhere in your script or loaded from a file
 top_features = ['MonthlyCharges', 'tenure', 'TotalCharges', 'Contract',
@@ -18,10 +25,15 @@ def preprocess_input(user_input):
     # Assuming user_input is a dictionary with keys as column names
     input_df = pd.DataFrame([user_input])
     
-    # Apply label encoding to categorical columns
-    for col in input_df.select_dtypes(include=['object']).columns:
-        input_df[col] = label_encoder.transform(input_df[col])
-    
+    # Check if label_encoder is fitted
+    if label_encoder is not None and hasattr(label_encoder, "classes_"):
+        # Apply label encoding to categorical columns
+        for col in input_df.select_dtypes(include=['object']).columns:
+            input_df[col] = label_encoder.transform(input_df[col])
+    else:
+        st.error("Label encoder is not fitted. Make sure to fit and save the label encoder during training.")
+        st.stop()
+
     # Scale numerical features using the previously trained scaler
     input_df[top_features] = scaler.transform(input_df[top_features])
 
@@ -50,3 +62,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
