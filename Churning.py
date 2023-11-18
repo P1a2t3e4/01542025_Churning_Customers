d43e1fa45b1a98be50a77a5558d51d8d4efba57c
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-18 10:51:00 +0000

    Update Churning.py

--- Churning.py
+++ Churning.py
@@ -1,60 +1,28 @@
 import streamlit as st
-import os
 import pickle
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-
-# Load the best model and preprocessing objects
-best_model = pickle.load(open("best_model .pkl", "rb"))
-scaler = pickle.load(open("scaler.pkl", "rb"))
-
+from sklearn.preprocessing import LabelEncoder
 
 # Load the saved label encoder
 label_encoder_file = "label_encoder.pkl"
 with open(label_encoder_file, 'rb') as file:
     label_encoder = pickle.load(file)
 
-# Now you can use label_encoder.transform in your Streamlit app
-
-
-# Encode categorical variables
-#label_encoder = LabelEncoder()
-categorical_columns = ['MonthlyCharges', 'tenure', 'TotalCharges', 'Contract',
-       'PaymentMethod', 'OnlineSecurity', 'TechSupport', 'gender',
-       'InternetService', 'OnlineBackup']
-        
-for column in categorical_columns:
-
-    user_input[column] = label_encoder.fit_transform(user_input[column])
-
-        # Scale the input
-    scaled_input = scaled.transform(user_input)
-
-
-
 # Assuming top_features is defined somewhere in your script or loaded from a file
 top_features = ['MonthlyCharges', 'tenure', 'TotalCharges', 'Contract',
-       'PaymentMethod', 'OnlineSecurity', 'TechSupport', 'gender',
-       'InternetService', 'OnlineBackup']  # Replace [...] with the actual definition or loading logic
+                'PaymentMethod', 'OnlineSecurity', 'TechSupport', 'gender',
+                'InternetService', 'OnlineBackup']  # Replace [...] with the actual definition or loading logic
 
 # Function to preprocess user input
 def preprocess_input(user_input):
-    # Assuming user_input is a dictionary with keys as column names
-    input_df = pd.DataFrame([user_input])
-
     # Check if label_encoder is fitted
     if label_encoder is not None and hasattr(label_encoder, "classes_"):
         # Apply label encoding to categorical columns
-        for col in input_df.select_dtypes(include=['object']).columns:
-            input_df[col] = label_encoder.transform(input_df[col])
-    else:
-        st.error("Label encoder is not fitted. Make sure to fit and save the label encoder during training.")
-        st.stop()
+        for col in top_features:
+            if col in user_input and col in label_encoder.classes_:
+                user_input[col] = label_encoder.transform([user_input[col]])[0]
 
-    # Scale numerical features using the previously trained scaler
-    input_df[top_features] = scaler.transform(input_df[top_features])
-
-    return input_df
+    return user_input
 
 # Streamlit app
 def main():
@@ -68,14 +36,11 @@ def main():
 
     if st.sidebar.button("Predict"):
         # Preprocess user input
-        input_df = preprocess_input(user_input)
+        input_df = pd.DataFrame([preprocess_input(user_input)])
 
-        # Make predictions using the best model
-        prediction = best_model.predict(input_df)
-
-        # Display the prediction
-        st.write("## Prediction")
-        st.write(f"The predicted churn status is: {prediction[0]}")
+        # Display the preprocessed input DataFrame
+        st.write("## Preprocessed Input Data")
+        st.write(input_df)
 
 if __name__ == "__main__":
     main()
