b1d3dfce5941078b8dafea5deef615eb58a469ea
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-18 09:03:44 +0000

    Update Churning.py

--- Churning.py
+++ Churning.py
@@ -1,66 +1,48 @@
-import streamlit as st
-import pandas as pd
-from keras.models import load_model
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+import requests
 import pickle
+import streamlit as st
+import numpy as np
+import pandas as pd
 
-# Load the trained model
-model = load_model('best_model.h5')
+with open(r'best_model .pkl','rb') as file:
+    best_module = pickle.load(file)
+with open(r'scaler.pkl','rb') as file:
+    scaler_module = pickle.load(file)
 
-# Load the trained StandardScaler
-with open('scaler.pkl', 'rb') as file:
-    scaled = pickle.load(file)
+st.set_page_config(
+    page_title="CUSTOMER CHURN PREDICTIONS",
+    page_icon=":wave:",
+    layout="centered",
+    initial_sidebar_state="expanded",
+  
+)
 
-# Streamlit app
-def main():
-    st.title('Churn Prediction App')
+st.sidebar.header("Enter Customer Details", divider='rainbow')
 
-    # Collect user input
-    tenure = st.slider('Tenure', 0, 70, 20)
-    monthly_charges = st.slider('Monthly Charges', 0.0, 200.0, 100.0)
-    total_charges = st.slider('Total Charges', 0.0, 5000.0, 2500.0)
-    contract = st.selectbox('Contract', ['month-to-month', 'one year', 'Two years'])
-    online_security = st.selectbox('Online Security', ['No', 'yes', 'No internet service'])
-    payment_method = st.selectbox('Payment Method', ['electronic check', 'mailed check', 'card'])
-    tech_support = st.selectbox('Tech Support', ['No', 'yes', 'No internet service'])
-    internet_service = st.selectbox('Internet Service', ['DSL', 'fiber optic', 'No'])
-    online_backup = st.selectbox('Online Backup', ['No', 'yes', 'No internet service'])
-    gender = st.radio('Gender', ['Male', 'Female'])
+gender = st.radio("What is your gender","Male","Female")
+SeniorCitizen = st.radio("Are you a senior citizen ","YES","NO")
+tenure = st.slider("How long have you been a customer",1,100)
+InternetService = st.radio("What is your internet service","DSL","Fiber optic","No")
+OnlineSecurity = st.radio("Do you have online security","YES","NO")
+OnlineBackup = st.radio("Do you have online backup","YES","NO")
+TechSupport = st.radio("Do you have tech support","YES","NO")
+Contract = st.radio("What is your contract","Month-to-month","One year","Two year")
+PaymentMethod = st.radio("What is your payment method","Electronic check","Mailed check","Bank transfer (automatic)","Credit card (automatic)")
+MonthlyCharges = st.slider("What is your monthly charges",0,120)
+TotalCharges = st.slider("What is your total charges",0,10000)
+submit =st.button("Submit")
+user_response = [gender,SeniorCitizen,tenure,InternetService,OnlineSecurity,
+                 OnlineBackup,TechSupport,Contract,PaymentMethod,MonthlyCharges,TotalCharges]
 
-    # Make a prediction
-    if st.button('Predict Churn'):
-        # Transform user input
-        user_input = pd.DataFrame({
-            'tenure': [tenure],
-            'MonthlyCharges': [monthly_charges],
-            'TotalCharges': [total_charges],
-            'Contract': [contract],
-            'OnlineSecurity': [online_security],
-            'PaymentMethod': [payment_method],
-            'TechSupport': [tech_support],
-            'InternetService': [internet_service],
-            'OnlineBackup': [online_backup],
-            'gender': [gender]
-        })
-
-        # Encode categorical variables
-        label_encoder = LabelEncoder()
-        categorical_columns = ['Contract', 'OnlineSecurity', 'PaymentMethod', 'TechSupport', 'InternetService', 'OnlineBackup', 'gender']
-        
-        for column in categorical_columns:
-            user_input[column] = label_encoder.fit_transform(user_input[column])
-
-        # Scale the input
-        scaled_input = scaled.transform(user_input)
-
-        # Make a prediction
-        prediction = model.predict(scaled_input)
-
-        # Display the result
-        churn_probability = prediction[0]
-        churn_prediction = 'Yes, Customer will Churn' if churn_probability >= 0.5 else 'Customer will not Churn'
-        st.write(f'Churn Probability: {churn_probability}')
-        st.write(f'Prediction: {churn_prediction}')
-
-if __name__ == '__main__':
-    main()
+attributes = ['gender', 'SeniorCitizen', 'tenure', 'InternetService',
+       'OnlineSecurity', 'OnlineBackup', 'TechSupport', 'Contract',
+       'PaymentMethod', 'MonthlyCharges', 'TotalCharges']
+if submit:
+        newData = pd.DataFrame([user_response],columns= attributes)
+        scaledData = scaler_module.transform(newData)
+        predict = best_module.predict(scaledData)
+        print(scaledData)
+        st.subheader("The customer churn rate is "+str( round(predict[0])),divider='rainbow')
+        print(predict[0]+(0.8809915725973115*1.96))
+        print(predict[0]+(0.8809915725973115-1.96))
+        st.text("There's a 95% chance that the rating is between " + str(round(predict[0]+(0.8809915725973115-1.96))) +" and "+str(round(predict[0]+(0.8809915725973115*1.96))))
