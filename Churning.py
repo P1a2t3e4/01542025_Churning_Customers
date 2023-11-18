bdd47f8148870fc13bad599442a6a8821e51f2a6
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-18 10:05:36 +0000

    Update Churning.py

--- Churning.py
+++ Churning.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Load the best model and preprocessing objects
-best_model = pickle.load(open("best_model.pkl", "rb"))
+best_model = pickle.load(open("best_model .pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 label_encoder = pickle.load(open("label_encoder.pkl", "rb"))
 
