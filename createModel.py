69bb1f56060862b71038403e070391b1a478e516
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-18 08:50:04 +0000

    Add files via upload

---
+++ createModel.py
@@ -0,0 +1,23 @@
+
+def create_model(dropout_rate, weight_constraint,neurons,activation,num_classes,X_Corr):
+  # create modeloptimizer=optimizer
+  input_shape = (X_Corr.shape[1],)
+  inputs = tf.keras.Input(shape=input_shape)
+  input = tf.keras.layers.Dense((28)+neurons, activation=activation)(inputs)
+  x= tf.keras.layers.Dropout(dropout_rate)(input)
+  second=tf.keras.layers.Dense((12)+neurons, activation=activation)(x)
+  x= tf.keras.layers.Dropout(dropout_rate)(second)
+  third=tf.keras.layers.Dense((4)+neurons, activation=activation)(x)
+  x= tf.keras.layers.Dropout(dropout_rate)(third)
+  fourth=tf.keras.layers.Dense((-4)+neurons, activation=activation)(x)
+  x= tf.keras.layers.Dropout(dropout_rate)(fourth)
+  fifth=tf.keras.layers.Dense((-12)+neurons, activation=activation)(x)
+# Add more hidden layers if necessary
+
+# Add output layer with softmax activation
+  outputs = tf.keras.layers.Dense(num_classes, activation='sigmoid')(fifth)
+
+# Create the model
+  model_optim = tf.keras.Model(inputs=inputs, outputs=outputs)
+  model_optim.compile(loss='binary_crossentropy', optimizer='SGD', metrics=['accuracy'])
+  return  model_optim
