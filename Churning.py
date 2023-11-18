1e22d38a78d41644462444e9a7efdda2aed0e8b4
PatriciaOkletey <113718869+P1a2t3e4@users.noreply.github.com>
2023-11-18 09:34:00 +0000

    Update Churning.py

--- Churning.py
+++ Churning.py
@@ -43,10 +43,10 @@ attributes = ['MonthlyCharges', 'tenure', 'TotalCharges', 'Contract',
        'InternetService', 'OnlineBackup']
 if submit:
         
-    Assuming numerical_columns contains the names of numerical columns
+    #Assuming numerical_columns contains the names of numerical columns
         newData[numerical_columns] = newData[numerical_columns].apply(pd.to_numeric, errors='coerce')
         
-        Assuming original_data_shape is a tuple representing the shape of the original data
+       # Assuming original_data_shape is a tuple representing the shape of the original data
         if newData.shape[1] != original_data_shape[1]:
             raise ValueError("Number of columns in newData does not match the original data.")
 
